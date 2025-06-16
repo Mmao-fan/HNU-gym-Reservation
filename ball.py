f7c5bcb6f1311508479bb16bf7278d1f3f1efb83
maozhufoan <32026041+Mmao-fan@users.noreply.github.com>
2025-06-16 23:44:41 +0800

    Add files via upload

--- ball.py
+++ ball.py
@@ -21,24 +21,24 @@ def parse_cookie_values(text):
     return result
 headers = {
         'accept': 'application/json, text/plain, */*',
-        'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
         'cache-control': 'no-cache',
         'content-type': 'application/x-www-form-urlencoded',
         'origin': 'https://eportal.hnu.edu.cn',
         'pragma': 'no-cache',
         'priority': 'u=1, i',
-        'referer': 'https://eportal.hnu.edu.cn/v2/reserve/reserveDetail?id=83',
-        'sec-ch-ua': '"Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
+        'referer': 'https://eportal.hnu.edu.cn/v2/reserve/reserveDetail?id=57',
+        'sec-ch-ua': '"Chromium";v="130", "Microsoft Edge";v="130", "Not?A_Brand";v="99"',
         'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"macOS"',
+        'sec-ch-ua-platform': '"Windows"',
         'sec-fetch-dest': 'empty',
         'sec-fetch-mode': 'cors',
         'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0',
         'x-requested-with': 'XMLHttpRequest',
 }
 raw = """
-//   -b 'UM_distinctid=191bcfd51b1970-022323237a-4c657b58-1bcab9-191bcfd51b2103f; PHPSESSID=ST-36472323-efZoJ7ReP0N232H-zfsoftcom; vjuid=2323; vjvd=3e159fbcc098c33132fa6da01; vt=264323832; cas_ticket=ST-3647798-efZoJ7R3233vKH-zfsoft.com' \
+//   -b 'PHPSESSID cas_ticket=ST-4584132-gPZR4qmfnzlPdwKxgtIT-zfsoft.com' \
 """
 cookies = parse_cookie_values(raw)
 # 计算明天的日期
@@ -46,24 +46,25 @@ tomorrow = date.today() + timedelta(days=1)
 
 # 按 YYYY-MM-DD 格式生成字符串并赋值
 datas = tomorrow.strftime("%Y-%m-%d")
-def get_time(start_time,select,num,times=1):
+def get_time(start_time,select,num,times):
     while True:
         if select==0:
             base_head=57
-            base_time=3885
-            base_id=16339
+            base_time=4431
+            base_id=20836
         elif select==1:
             base_head=85
-            base_time=4075
-            base_id=18917
-        data = {
+            base_time=4405
+            base_id=20656
+        if times==1:
+            data = {
             "resource_id": f"{base_head}",
             "code": "",
             "remarks": "",
             "deduct_num": "",
             "data" : f'[{{"date":"{datas}","period":{start_time+base_time},"sub_resource_id":{14*num+base_id-start_time}}}]'
-        }
-        if times==2:
+            }
+        elif times==2:
             data = {
                 "resource_id": f"{base_head}",
                 "code": "",
@@ -83,7 +84,7 @@ def get_current_minute():
 def get_current_second():
     return datetime.datetime.now().strftime("%S")
 if __name__ == "__main__":
-    hour,min,sec=22,1,0
+    hour,min,sec=22,0,1
     while True:
         h=get_current_hour()
         m=get_current_minute()
@@ -94,4 +95,5 @@ if __name__ == "__main__":
             print(f"当前时间：{h}:{m}:{get_current_second()}")
             time.sleep(0.01)
     while True:
-        get_time(13,1,1,1)
+        get_time(8,0,6,2)
+        #开始时间，场馆0下1上，场号，抢几个时间段
